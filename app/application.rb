aeffebf1ed613de6d7e70da9f042d1b0e3b9ff48
github username <github email address>
2020-02-17 19:34:49 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,26 @@
+# require 'pry'
+
+class Application
+
+    def call(env)
+      resp = Rack::Response.new
+      time = Time.now.to_s.split(" ")
+      time = time[1].split(":")
+      time = time[0].to_i
+      
+      if time < 12
+        resp.write "Good Morning!"
+      else 
+        resp.write "Good Afternoon!"
+      end 
+
+      resp.finish
+    end
+  
+  end
+
+
+
+
+# start = Application.new
+# start.time
